1a21fcbe6a7a478752fa536ccbdcc6cb74b23ff9
hex1001 <hex1001@mail.ru>
2022-04-09 12:21:58 +0500

    Icon added to resource

--- microCalcium/Properties/Resources.Designer.cs
+++ microCalcium/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace microCalcium.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -217,6 +217,16 @@ namespace microCalcium.Properties {
             get {
                 object obj = ResourceManager.GetObject("mill2_on_left", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon options_icon {
+            get {
+                object obj = ResourceManager.GetObject("options_icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
             }
         }
         
